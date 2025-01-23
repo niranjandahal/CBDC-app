8def894c0f67a9607351ccd86fafa3c9343d14ea
niranjandahal <niranjandahal76@gmail.com>
2025-01-23 20:02:48 +0545

    changes for production

--- lib/main.dart
+++ lib/main.dart
@@ -68,6 +68,8 @@
 //   }
 // }
 
+//only for deployement purpose
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shared_preferences/shared_preferences.dart';
